b81b6a69d59cd1b9e709a40d4768a032591e8446
JeliHacker <jeligooch@gmail.com>
2019-10-10 00:33:55 -0400

    how it do baby

---
+++ myscripts.js
@@ -0,0 +1,5 @@
+
+
+document.getElementById("info").append("The pros of a times series chart is that is shows changes in data across a period of time. These charts, however, can still be misleading. Cairo warns against paradoxes and mix effects in time series charts and contends that");
+
+document.getElementById("difference").append("The fundamental difference between the data discussed in chapter 7 and the data discussed in chapter 8 is that the data in chapter 7 concerns distributions and how they are visualized so that they better represent the situation. Chapter 8 is concerned with revealing change in data in ways that aren't misleading.")
